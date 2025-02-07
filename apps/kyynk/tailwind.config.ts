994471f1fa7db84a45ee9516cc912cea2745ad43
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-02-07 11:01:33 +0100

    create src folder

--- apps/kyynk/tailwind.config.ts
+++ apps/kyynk/tailwind.config.ts
@@ -3,10 +3,10 @@ import sharedConfig from '@kyynk/tailwind-config';
 
 const config: Pick<Config, 'content' | 'presets'> = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './shared/**/*.{js,ts,jsx,tsx,mdx}',
-    './app-features/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/shared/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app-features/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   presets: [sharedConfig],
 };
