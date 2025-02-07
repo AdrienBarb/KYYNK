5de91539331310f3ca2f76a4852823ec6c1a9bf6
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-02-07 12:49:43 +0100

    fix import

--- apps/kyynk/src/app/(app)/account/parameters/my-account/edit/email/verification/page.tsx
+++ apps/kyynk/src/app/(app)/account/parameters/my-account/edit/email/verification/page.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import { useTranslations } from "next-intl";
-import EmailVerification from "@/components/EmailVerification";
-import CenterHeader from "@/components/CenterHeader";
-import PaddingContainer from "@/components/PaddingContainer";
+import React from 'react';
+import { useTranslations } from 'next-intl';
+import EmailVerification from '@/components/EmailVerification';
+import PaddingContainer from '@/components/PaddingContainer';
 
 const CreatorEmailConfirmationPage = () => {
   const t = useTranslations();
 
   return (
     <PaddingContainer>
-      <CenterHeader tag="h2" title={t("common.confirmEmail")} />
       <EmailVerification
-        nextPath={"/dashboard/account/parameters/my-account"}
+        nextPath={'/dashboard/account/parameters/my-account'}
       />
     </PaddingContainer>
   );
--- apps/kyynk/src/components/NavLogo.tsx
+++ apps/kyynk/src/components/NavLogo.tsx
@@ -6,7 +6,7 @@ import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { FC } from 'react';
-import logo from '@/public/images/logo.svg';
+import logo from '../../public/images/logo.svg';
 
 interface Props {}
 
