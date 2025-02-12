daae17f9653175f764053b140837eb8c3526b810
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-02-12 14:12:35 +0100

    wip

--- apps/kyynk/src/data/siteMetadata.js
+++ apps/kyynk/src/data/siteMetadata.js
@@ -1,18 +1,17 @@
 /** @type {import("pliny/config").PlinyConfig } */
-const siteMetadata = {
-  title: "KYYNK",
+
+export default {
+  title: 'KYYNK',
   description:
-    "Get closer than ever to the hottest creators. Unlock and enjoy exclusive content, connect directly with creators, and more.",
-  language: "en-us",
-  siteUrl: "https://www.kyynk.com",
-  siteLogo: "/images/logo-kyynk.jpg",
-  socialBanner: "/images/logo-kyynk.jpg",
-  locale: "en-US",
+    'Get closer than ever to the hottest creators. Unlock and enjoy exclusive content, connect directly with creators, and more.',
+  language: 'en-us',
+  siteUrl: 'https://www.kyynk.com',
+  siteLogo: '/images/logo-kyynk.jpg',
+  socialBanner: '/images/logo-kyynk.jpg',
+  locale: 'en-US',
   analytics: {
     googleAnalytics: {
-      googleAnalyticsId: "", // e.g. G-XXXXXXX
+      googleAnalyticsId: '', // e.g. G-XXXXXXX
     },
   },
 };
-
-module.exports = siteMetadata;
