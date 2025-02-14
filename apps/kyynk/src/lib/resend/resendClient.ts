c0e11416d62aca6b1ba0db19f686e06fc0bb37d3
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-02-14 16:30:29 +0100

    wip

--- apps/kyynk/src/lib/resend/resendClient.ts
+++ apps/kyynk/src/lib/resend/resendClient.ts
@@ -1,5 +1,7 @@
 import { Resend } from 'resend';
 
-export const resendClient = process.env.RESEND_API_KEY
-  ? new Resend(process.env.RESEND_API_KEY)
-  : null;
+console.log('🚀 ~ process.env.RESEND_API_KEY:', process.env.RESEND_API_KEY);
+
+export const resendClient = new Resend(
+  process.env.RESEND_API_KEY ? process.env.RESEND_API_KEY : '',
+);
