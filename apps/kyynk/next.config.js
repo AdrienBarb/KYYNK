b3ba759c050b283b4b3dae3c89005ccc9c248f2d
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-02-14 15:08:04 +0100

    wip

--- apps/kyynk/next.config.js
+++ apps/kyynk/next.config.js
@@ -23,9 +23,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-  },
 };
 
 export default withNextIntl(nextConfig);
--- apps/kyynk/src/lib/resend/resendClient.ts
+++ apps/kyynk/src/lib/resend/resendClient.ts
@@ -1,7 +1,4 @@
 import { Resend } from 'resend';
 
-console.log(
-  '🚀 ~ process.env.RESEND_API_KEY:',
-  process.env.NEXT_PUBLIC_RESEND_API_KEY,
-);
-export const resendClient = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+console.log('🚀 ~ process.env.RESEND_API_KEY:', process.env.RESEND_API_KEY);
+export const resendClient = new Resend(process.env.RESEND_API_KEY);
