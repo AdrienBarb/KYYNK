ba4e7eca62837e8ff5c38f21bd3285f7f4ca7d9e
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-02-14 08:23:41 +0100

    Enable posthog in prod

--- apps/kyynk/src/components/provider/PostHogProvider.tsx
+++ apps/kyynk/src/components/provider/PostHogProvider.tsx
@@ -4,13 +4,21 @@ import posthog from 'posthog-js';
 import { PostHogProvider as PHProvider } from 'posthog-js/react';
 import { useEffect } from 'react';
 import PostHogPageView from '@/components/tracking/PostHogPageView';
+
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      person_profiles: 'always',
-      capture_pageview: true,
-    });
+    console.log(
+      "🚀 ~ useEffect ~ process.env.NODE_ENV === 'production':",
+      process.env.NODE_ENV === 'production',
+    );
+    if (process.env.NODE_ENV === 'production') {
+      console.log('🚀 ~ Posthog is enabled');
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        person_profiles: 'always',
+        capture_pageview: true,
+      });
+    }
   }, []);
 
   return (
