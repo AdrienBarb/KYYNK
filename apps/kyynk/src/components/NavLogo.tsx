fcad3472f2d7afcabaf6f7bbb2772698e4cbc0f0
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-02-16 18:51:02 +0100

    wip

--- apps/kyynk/src/components/NavLogo.tsx
+++ apps/kyynk/src/components/NavLogo.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { FC } from 'react';
 import logo from '../../public/images/logo.svg';
+import { appRouter } from '@/constants/appRouter';
 
 interface Props {}
 
@@ -13,7 +14,7 @@ const NavLogo: FC<Props> = ({}) => {
 
   return (
     <Link
-      href={status === 'authenticated' ? '/dashboard/feed' : '/'}
+      href={status === 'authenticated' ? appRouter.models : appRouter.home}
       passHref
       prefetch
     >
