254093999421057f549d3520e9fddfdf76562bbd
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-04-05 09:34:22 +0200

    fix env

--- apps/kyynk/src/utils/environments/index.ts
+++ apps/kyynk/src/utils/environments/index.ts
@@ -1,5 +1,5 @@
-export const isProduction = process.env.APP_ENV === 'production';
-export const isStaging = process.env.APP_ENV === 'staging';
-export const isDevelopment = process.env.APP_ENV === 'development';
+export const isProduction = process.env.NEXT_PUBLIC_APP_ENV === 'production';
+export const isStaging = process.env.NEXT_PUBLIC_APP_ENV === 'staging';
+export const isDevelopment = process.env.NEXT_PUBLIC_APP_ENV === 'development';
 
-console.log(`Running in ${process.env.APP_ENV} mode.`);
+console.log(`Running in ${process.env.NEXT_PUBLIC_APP_ENV} mode.`);
