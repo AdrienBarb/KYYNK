41afdd1395313c4b3e9ce7d665ae5d9c03977d42
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-05-30 10:35:58 +0200

    remove darkmode

--- apps/kyynk/tailwind.config.ts
+++ apps/kyynk/tailwind.config.ts
@@ -2,8 +2,7 @@ import type { Config } from 'tailwindcss';
 import sharedConfig from '@kyynk/tailwind-config';
 
 const config: Pick<Config, 'content' | 'presets'> = {
-    darkMode: ['class'],
-    content: [
+  content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
