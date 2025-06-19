0ae4bfd56eb8ebc330923b98a7ff985f3e5b34e0
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-06-19 07:54:55 +0200

    update posthog client

--- apps/kyynk/src/components/provider/PostHogProvider.tsx
+++ apps/kyynk/src/components/provider/PostHogProvider.tsx
@@ -14,7 +14,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
         person_profiles: 'identified_only',
         capture_pageview: true,
         capture_pageleave: true,
-        autocapture: true,
+        autocapture: false,
       });
     }
   }, []);
